bb4e9f8ced43633972acb64062a071fcc933c389
yasmeen_omar <yasmeen_omar@bitbucket.org>
2023-11-26 01:17:46 +0300

    initial commit

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import helmet from 'helmet';
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    cors: true,
-  });
+  const app = await NestFactory.create(AppModule);
+  app.use(helmet());
   app.enableCors({
     origin: '*',
     allowedHeaders: '*',
