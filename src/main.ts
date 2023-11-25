ad6e324bfcc8fd9840f82abb28d54340963c8496
yasmeen_omar <yasmeen_omar@bitbucket.org>
2023-11-26 01:12:24 +0300

    initial commit

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    // origin: '*',
-    // allowedHeaders: '*',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    allowedHeaders: ['content-type'],
-    origin: 'http://localhost:3000',
-    credentials: true,
+    origin: '*',
+    allowedHeaders: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   });
   app.enableCors();
   await app.listen(3000);
