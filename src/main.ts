4f678dc6d88d7db78067a6de5b64f40efd732f5e
yasmeen_omar <yasmeen_omar@bitbucket.org>
2023-12-16 22:20:04 +0300

    updated src/main.ts"

--- src/main.ts
+++ src/main.ts
@@ -8,9 +8,15 @@ async function bootstrap() {
   app.use(helmet());
 
   app.enableCors({
-    origin: '*', // Update this to the specific origins you want to allow in production
-    allowedHeaders: '*', // Update this to the specific allowed headers
+    origin: [
+      'https://api-oo2i5mgod-yasmeenokh.vercel.app',
+      'http://localhost:3000',
+      '*',
+    ], // Update this to the specific origins you want to allow in production
+    // allowedHeaders: '*', // Update this to the specific allowed headers
     methods: 'GET,PUT,PATCH,POST,DELETE',
+    allowedHeaders:
+      'Origin, X-Requested-With, Content-Type, Accept, Authentication, Access-control-allow-credentials, Access-control-allow-headers, Access-control-allow-methods, Access-control-allow-origin, User-Agent, Referer, Accept-Encoding, Accept-Language, Access-Control-Request-Headers, Cache-Control, Pragma',
   });
 
   await app.listen(4000);
